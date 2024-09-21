08db70b6e53ecde6db5be19a15710ee5ea9385ec
Iskander Gaba <iskander@hey.com>
2024-09-21 18:59:22 +0200

    Update bot so that it runs at least

--- src/bot.py
+++ src/bot.py
@@ -1,38 +1,26 @@
-import notify2
-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from bs4 import BeautifulSoup
-from requests import get
 from os.path import abspath
 from time import sleep
+
 from playsound import playsound
+from selenium import webdriver
 
 url = "http://www.isere.gouv.fr/booking/create/23012/0"
 url_reject = "http://www.isere.gouv.fr/booking/create/23012/2"
 
-notification = notify2.Notification("Summary111",
-                "Some body text111",
-            #  "notification-message-im"   # Icon name
-            )
-notification.set_urgency(notify2.URGENCY_CRITICAL)
-
-notification_reject = notify2.Notification("Summary",
-                "Some body text",
-            #  "notification-message-im"   # Icon name
-            )
 
 class Requestxception(Exception):
     pass
+
 
 def assert_error(driver):
     if "502 Bad Gateway" in driver.page_source:
         driver.get(url)
         raise Requestxception
 
+
 def open_browser():
-    notify2.init("Titre de Séjour Bot")
-    driver = webdriver.Firefox(executable_path=abspath("geckodriver"))
+    print("Titre de Séjour Bot")
+    driver = webdriver.Firefox()
     driver.get(url)
     assert_error(driver)
     driver.execute_script("javascript:accepter()")
@@ -43,24 +31,24 @@ def open_browser():
         except Requestxception:
             driver.refresh()
             continue
-        driver.find_element_by_id('condition').click()
-        driver.find_element_by_name('nextButton').click()
+        driver.find_element_by_id("condition").click()
+        driver.find_element_by_name("nextButton").click()
         try:
             assert_error(driver)
         except Requestxception:
             driver.refresh()
             continue
         if driver.current_url == url_reject:
-            # notification_reject.show()
-            driver.find_element_by_name('finishButton').click()
+            driver.find_element_by_name("finishButton").click()
         else:
-            notification.show()
             break
     playsound(abspath("silent_hill_siren.mp3"))
-    # driver.quit()
+    driver.quit()
+
 
 def main():
-    print(open_browser())
+    open_browser()
+
 
 if __name__ == "__main__":
     main()
